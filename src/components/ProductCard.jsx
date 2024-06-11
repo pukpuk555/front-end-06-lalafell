c3934eb71188233086eab6c2d7488c44edecd525
pukpuk555 <jester2540@gmail.com>
2024-06-11 16:24:44 +0700

    add picture and object

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ProductCard() {
+function ProductCard({ name, img, price, describe }) {
   return (
     <a href="#">
       <div className="h-auto min-h-40 md:h-[300px]  transition duration-500 hover:scale-110">
@@ -8,20 +8,22 @@ function ProductCard() {
           <img
             className="rounded-md h-[150px] md:h-[200px] md:w-[250px] md:object-cover"
             //for test
-            src="/kimetsu_no.png"
+            src={img}
           />
         </div>
-        <div className="flex flex-col justify-between md:h-[100px]">
-          <h4 className="md:text-xl font-bold text-sm">
-            {/* for test */}
-            Kimetsu no Yaiba Custom
-          </h4>
+        <div className="flex flex-col justify-between h-[60px] md:h-[100px]">
+          <div className="w-[150px] md:w-[250px] flex flex-nowrap">
+            <h4 className="md:text-xl font-bold text-sm">
+              {/* for test */}
+              {name}
+            </h4>
+          </div>
           <p className="text-m hidden md:flex">
             {/*for test*/}
-            Reference from popular animation
+            {describe}
           </p>
           {/* for test */}
-          <h5 className="md:text-l font-semibold text-sm">$10.99</h5>
+          <h5 className="md:text-l font-semibold text-sm">{price}</h5>
         </div>
       </div>
     </a>
--- src/components/WhatsNewSection.jsx
+++ src/components/WhatsNewSection.jsx
@@ -4,27 +4,62 @@ import ProductCard from "./ProductCard";
 function WhatsNewSection() {
   //for test
   const whatNewArray = [
-    { ProductCard },
-    { ProductCard },
-    { ProductCard },
-    { ProductCard },
-    { ProductCard },
-    { ProductCard },
+    {
+      name: "Kimetsu no Yaiba Custom",
+      describtion: "Reference from popular animation",
+      img: "/kimetsu_no.png",
+      price: "$10.99",
+    },
+    {
+      name: "Lupan The 3rd Custom",
+      describtion: "Reference from popular animation",
+      img: "/lupin.png",
+      price: "$10.99",
+    },
+    {
+      name: "Doreaman Custom",
+      describtion: "Reference from popular animation",
+      img: "/doremon.png",
+      price: "$10.99",
+    },
+    {
+      name: "Harry Poster Custom",
+      describtion: "Reference from popular animation",
+      img: "/harry.png",
+      price: "$10.99",
+    },
+    {
+      name: "Jin-Chan Custom",
+      describtion: "Reference from popular animation",
+      img: "/chinchan.png",
+      price: "$10.99",
+    },
+    {
+      name: "Pokeman Custom",
+      describtion: "Reference from popular animation",
+      img: "/pokemon.png",
+      price: "$10.99",
+    },
   ];
 
   return (
     <div className="flex justify-center w-screen">
-      <div className="w-[1024px]">
+      <div className="w-screen md:w-[1024px]">
         <h3 className="text-xl md:text-3xl font-bold">What's New</h3>
         <div className="flex md:justify-center">
-          <div className="carousel carousel-center p-4 space-x-4 md:grid md:grid-cols-3 md:grid-row-2 md:gap-5 md:mx-6 md:mt-4">
-            {whatNewArray.map((card) => (
+          <div className="carousel carousel-center gap-5 p-4 md:grid md:grid-cols-3 md:grid-row-2 md:mx-6 w-auto">
+            {whatNewArray.map((card, index) => (
               <div
-                key={card.id}
+                key={index}
                 className="carousel-item md:flex md:justify-center"
               >
                 {/* for test */}
-                <ProductCard />
+                <ProductCard
+                  name={card.name}
+                  img={card.img}
+                  price={card.price}
+                  describe={card.describtion}
+                />
               </div>
             ))}
           </div>

17a534770366dc101d8b951f867419fed1dd5acd
ApPond <you@example.com>
2024-06-12 09:57:55 +0700

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-function App() {
+import React, { useState } from 'react';
+import FooterSection from './components/FooterSection';
+import ContactButton from './components/ContactButton';
+import ContactForm from './components/ContactForm';
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        {/* Add your main content here */}
+        <h1 className="text-center mt-4">Welcome to Lalafell Keyboard</h1>
+        {/* You can add more content here */}
+      </div>
+      <FooterSection />
+      <ContactButton onClick={toggleModal} />
+      {isModalOpen && <ContactForm onClose={toggleModal} />}
+    </div>
   );
-}
+};
 
 export default App;
+

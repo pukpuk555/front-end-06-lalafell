1ba2d8c91a9ba29d5c96211de6554959568357cc
Soravit Sakukumpat <dareal.u.ess@gmail.com>
2024-06-21 21:03:52 +0700

    ส่งไปบานตัวเอง

--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -121,7 +121,7 @@ export default function SignUp() {
           <div className="py-3">
             <button
               type="submit"
-              className="border border-black-500 rounded-lg py-2 w-[400px] bg-slate-100"
+              className="border border-black-500 rounded py-2 w-[400px] bg-gray-300 hover:bg-gray-500"
             >
               Sign up with Email
             </button>
@@ -135,11 +135,29 @@ export default function SignUp() {
             <div className="py-3">
             <a href="www.google.co.th" target="_blank">
             <button
-              type="button"
-              className="border border-black-500 rounded-lg py-2 w-[400px] text-lg font-semibold bg-slate-200"
-            > 
-              Google
-            </button>
+            type="button"
+            className="flex items-center justify-center px-4 py-2 mb-3 bg-gray-300 hover:bg-gray-500 rounded font-medium w-[400px]"
+          >
+            <svg className="w-[14px] h-[14px] mr-1" viewBox="0 0 48 48">
+              <path
+                fill="#4285F4"
+                d="M24 9.5c3.9 0 6.8 1.7 8.4 3.1l6.2-6.2C35.4 2.6 30.1 0 24 0 14.5 0 6.5 5.8 2.9 14.2l7.3 5.7C12.1 13.2 17.6 9.5 24 9.5z"
+              />
+              <path
+                fill="#34A853"
+                d="M46.1 24.6c0-1.3-.1-2.2-.2-3.2H24v7.2h12.9c-.5 3.4-2.2 5.8-4.9 7.5l7.4 5.7c4.3-3.9 6.7-9.6 6.7-17.2z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M10.2 28.6c-.7-1.2-1.1-2.6-1.1-4 0-1.4.4-2.8 1-4l-7.3-5.7c-1.6 3.1-2.6 6.6-2.6 10.7s1 7.6 2.6 10.7l7.4-5.7z"
+              />
+              <path
+                fill="#EA4335"
+                d="M24 48c6.5 0 11.9-2.1 15.8-5.6l-7.4-5.7c-2.1 1.4-4.8 2.3-8.4 2.3-6.5 0-12-4.3-14-10.2l-7.3 5.7C6.5 42.3 14.5 48 24 48z"
+              />
+            </svg>
+            Google
+          </button>
             </a>
           </div>
           <div>

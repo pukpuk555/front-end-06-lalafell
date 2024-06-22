bef17e06ef2b75ecf14ebb425c0713972cda199c
ApPond <you@example.com>
2024-06-22 18:47:02 +0700

    edit-path components

--- src/page/AccountPage.jsx
+++ src/page/AccountPage.jsx
@@ -2,7 +2,7 @@ import AccountNav from "@/components/MyAccount/AccountNav";
 import ContactButton from "@/components/ContactBotton/ContactButton";
 import FooterSection from "@/components/FooterSection";
 import NavbarSection from "@/components/NavbarSection";
-import ProfileCard from "@/components/ProfileCard";
+import ProfileCard from "@/components/MyAccount/ProfileCard";
 import React, { useState } from "react";
 
 function AccountPage() {

8b3ae6f69a1392f996eb779ac46502b612d6521a
ApPond <you@example.com>
2024-07-14 23:43:45 +0700

    Push P'Ying

--- src/components/ProductPage/ProductDetailCard.jsx
+++ src/components/ProductPage/ProductDetailCard.jsx
@@ -35,7 +35,7 @@ function ProductDetailCard({
     const totalPrice = product.price * quantity;
     const newProduct = {
 
-      product: [{ product: product.productId, quantity, price: product.price*quantity}],
+      product: [{ product: product.productId, quantity, price: product.price}],
       totalPrice,
 
     }

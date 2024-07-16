fb4f97a1828f047b2b3d36cd3e702655caea929c
ApPond <you@example.com>
2024-07-16 15:06:05 +0700

    add notification add to cart

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
 import "./index.css";
 import AccountPage from "./page/AccountPage";
@@ -15,10 +17,9 @@ import ForgotEmailorPassword from "./components/SignIn/ForgotEmailorPassword.jsx
 import PaymentStatusPage from "./page/PaymentStatusPage.jsx";
 import ProfileCard from "./components/MyAccount/ProfileCard.jsx";
 import AddressBook from "./components/MyAccount/AddressBook.jsx";
-import CrediCardList from "./components/MyAccount/CrediCardList.jsx";
+import CrediCardList from "./components/MyAccount/CrediCardList.jsx"; // Fixed typo here
 import OrderHistory from "./components/MyAccount/OrderHistory.jsx";
-import SignIn from "./page/SignInPage.jsx";
-import AdminPage from "./page/AdminPage.jsx"
+import AdminPage from "./page/AdminPage.jsx";
 import MyOrders from "./components/Admin/MyOrders.jsx";
 import Dashboard from "./components/Admin/Dashboard.jsx";
 
@@ -27,7 +28,7 @@ const whatNewArray = [
     name: "Kimetsu no Yaiba Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec: \n Keycap  \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec: \n Keycap  \n Switch Tactile with RGB \n Connection Wireless",
     img: "/kimetsu_no.png",
     price: "10.99",
   },
@@ -35,7 +36,7 @@ const whatNewArray = [
     name: "Lupan The 3rd Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Connection Wireless",
     img: "/lupin.png",
     price: "10.99",
   },
@@ -43,7 +44,7 @@ const whatNewArray = [
     name: "Doreaman Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Connection Wireless",
     img: "/doremon.png",
     price: "10.99",
   },
@@ -51,7 +52,7 @@ const whatNewArray = [
     name: "Harry Poster Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Connection Wireless",
     img: "/harry.png",
     price: "10.99",
   },
@@ -59,7 +60,7 @@ const whatNewArray = [
     name: "Jin-Chan Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Connection Wireless",
     img: "/chinchan.png",
     price: "10.99",
   },
@@ -67,7 +68,7 @@ const whatNewArray = [
     name: "Pokeman Custom",
     type: "Custom Keyboard",
     description: "Reference from popular animation",
-    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Conection Wireless",
+    spec: "Spec:\n Keycap \n Switch Tactile with RGB \n Connection Wireless",
     img: "/pokemon.png",
     price: "10.99",
   },
@@ -75,19 +76,18 @@ const whatNewArray = [
 
 const AppWrapper = () => {
   const [cart, setCart] = useState([]);
+
   const addToCart = (product, quantity) => {
-    // Check if the product already exists in the cart
     const index = cart.findIndex((item) => item.name === product.name);
 
     if (index !== -1) {
-      // Product already exists in cart, update its quantity
       const updatedCart = [...cart];
       updatedCart[index].quantity += quantity;
       setCart(updatedCart);
     } else {
-      // Product doesn't exist in cart, add it with the specified quantity
       setCart((prevCart) => [...prevCart, { ...product, quantity }]);
     }
+
   };
 
   return (
@@ -128,6 +128,8 @@ const AppWrapper = () => {
           <Route path="my-orders" element={<MyOrders />} />
         </Route>
       </Routes>
+      <ToastContainer
+      />
     </Router>
   );
 };
